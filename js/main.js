d78adb4b52a4ad3cf58a49aba4c6010dbd417af9
zubaer-khan <24850351+zubaer-khan@users.noreply.github.com>
2023-04-19 16:10:10 -0500

    Made changes to HTML and CSS to better optimize for JS

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-
+class Calculator{
+    constructor (getSum) {
+        this.sum = getSum;
+    }
+        add(){
+        let num = document.querySelector('button').addEventListener('click').value;
+        console.log(num)
+        }
+    
+}
