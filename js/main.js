5ca36363dcef272d15c88a944b96cf283a2c242a
zubaer-khan <24850351+zubaer-khan@users.noreply.github.com>
2023-04-20 23:45:30 -0500

    Worked on some of the JavaScript and changed some classes to data-types in HTML/CSS

--- js/main.js
+++ js/main.js
@@ -1,10 +1,29 @@
-class Calculator{
-    constructor (getSum) {
-        this.sum = getSum;
-    }
-        add(){
-        let num = document.querySelector('button').addEventListener('click').value;
-        console.log(num)
-        }
+const calculator = document.querySelector('.calculator')
+const keys = calculator.querySelector('.calculator__keys')
+const display = calculator.querySelector('.calculator__display');
+
+keys.addEventListener('click', event => {
+    if(!event.target.closest('button')) return;
+
+    const key = event.target;
+    const keyValue = key.textContent;
+    const displayValue = display.textContent;
+    const { type } = key.dataset;
+    const { previousKeyType } = calculator.dataset
     
-}
+    if (type === 'number'){
+        if (displayValue === '0'){
+            display.textContent = keyValue;
+        }else if (previousKeyType === 'operator') {
+            display.textContent = keyValue;
+        }else{
+            display.textContent += keyValue;
+        }
+    }
+
+    if (type === 'operator'){
+        console.log(key)
+    }
+
+    calculator.dataset.previousKeyType = type;
+})
